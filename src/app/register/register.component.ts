f1517caa3fa291b73dc6e78280126100f7d7b966
Seth herve <Riekhan@gmail.com>
2017-10-08 22:25:11 -0700

    adding missed file register.component.ts

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -7,23 +7,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  formBuilder: FormBuilder;
   userForm: FormGroup;
   loginCtrl: FormControl;
-  passwordCtrl: FormControl;
   birthYearCtrl: FormControl;
+  passwordForm: FormGroup;
+  passwordCtrl: FormControl;
+  confirmPasswordCtrl: FormControl;
 
-  constructor(fb: FormBuilder) {
-    this.loginCtrl = fb.control('', [Validators.required, Validators.minLength(3)]);
-    this.passwordCtrl = fb.control('', Validators.required);
-    this.birthYearCtrl = fb.control('', Validators.required);
-    this.userForm = fb.group({
-      login: this.loginCtrl,
-      password: this.passwordCtrl,
-      birthYear: this.birthYearCtrl
-    });
-   }
+  constructor(fb: FormBuilder) {}
+
+   static passwordMatch(control: FormGroup) {
+    const password = control.get('password').value;
+    const confirmPassword = control.get('confirmPassword').value;
+    return password !== confirmPassword ? { matchingError: true } : null;
+  }
+
+  static validYear(year) {
+    const currentYear = new Date().getFullYear();
+    return year > currentYear && year < 1900 ? {invalidYear: true} : null;
+  }
 
   ngOnInit() {
+    this.loginCtrl = this.formBuilder.control('', [Validators.required, Validators.minLength(3)]);
+    this.birthYearCtrl = this.formBuilder.control('', [Validators.required, RegisterComponent.validYear]);
+    this.passwordCtrl = this.formBuilder.control('', Validators.required);
+    this.confirmPasswordCtrl = this.formBuilder.control('', Validators.required);
+
+    this.passwordForm = this.formBuilder.group({
+      password: this.passwordCtrl,
+      confirmPassword: this.confirmPasswordCtrl
+    }, {validator: RegisterComponent.passwordMatch});
+
+    this.userForm = this.formBuilder.group({
+      login: this.loginCtrl,
+      password: this.passwordForm,
+      birthYear: this.birthYearCtrl
+    });
   }
 
   register() {
